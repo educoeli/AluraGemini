979024369af0f917a0668ee0f0d9707cf0650574
Eduardo Guimarães Coeli <edugcoeli@gmail.com>
2024-09-06 11:45:22 -0300

    inicio

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+    titulo: "Biologia Vegetal",
+    descricao: "A biologia vegetal, ou botânica, é a área da ciência que se dedica ao estudo das plantas. Essa área abrange desde a estrutura e funcionamento das células vegetais até a classificação e evolução das diversas espécies existentes.",
+    link : "https://www.superprof.com.br/blog/qual-a-importancia-da-biologia-vegetal/",
+    tags: "bio vegetal botanica celula"
+  },
+  {
+    titulo: "Biologia Animal",
+    descricao: "A biologia animal estuda os animais, desde os mais simples organismos unicelulares até os vertebrados mais complexos. Abrange a anatomia, fisiologia, comportamento e evolução dos animais.",
+    link: "https://www.nationalgeographic.com.br/animais/", // Link exemplo
+    tags: "bio animal organismo celula",
+},
+{
+    titulo: "Embriologia",
+    descricao: "A embriologia estuda o desenvolvimento de um embrião desde a fertilização até o nascimento. Abrange o crescimento, diferenciação celular e formação dos órgãos.",
+    link: "https://www.scielo.br/scielo.php?script=sci_arttext&pid=S0100-879X2003000300015", // Link exemplo
+    tags: "bio embriao formacao celula",
+},
+{
+    titulo: "Genética",
+    descricao: "A genética estuda a hereditariedade e a variação dos seres vivos. Investiga os genes, a transmissão de características hereditárias e as mutações.",
+    link: "https://www.nationalgeographic.com.br/ciencia/genetica/", // Link exemplo
+    tags: "genes genetica hereditario celula",
+},
+{
+    titulo: "Ecologia",
+    descricao: "A ecologia estuda as relações entre os seres vivos e o meio ambiente. Analisa as interações entre os organismos e os fatores abióticos, como clima e solo.",
+    link: "https://www.nationalgeographic.com.br/meio-ambiente/", // Link exemplo
+    tags: "eco meio ambente abiotico celula",
+},
+{
+    titulo: "Evolução",
+    descricao: "A evolução estuda as mudanças nas características hereditárias de populações de organismos ao longo das gerações. Abrange a seleção natural e a adaptação.",
+    link: "https://www.nationalgeographic.com.br/ciencia/evolucao/", // Link exemplo
+    tags: "evoluir populacao "
+}
+];  
